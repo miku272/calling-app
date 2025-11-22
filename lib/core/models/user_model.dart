9fd18f1a8a2fa01e84bac11864b97a77e0a7ad5d
Naresh Sharma <sharmanaresh272@gmail.com>
2025-11-22 22:23:08 +0530

    Modified user model

--- lib/core/models/user_model.dart
+++ lib/core/models/user_model.dart
@@ -7,10 +7,15 @@ part 'user_model.g.dart';
 abstract class UserModel with _$UserModel {
   const factory UserModel({
     required String uid,
-    required String email,
-    String? phoneNumber,
+    required String phoneNumber,
+    String? email,
     String? displayName,
-    String? photoUrl,
+    String? profileImageUrl,
+    @Default('Hey there! I am using Calling App.') String about,
+    @Default(false) bool isOnline,
+    DateTime? lastSeen,
+    String? pushToken,
+    DateTime? createdAt,
   }) = _UserModel;
 
   factory UserModel.fromJson(Map<String, dynamic> json) =>
